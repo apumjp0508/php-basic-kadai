23a9d1c8c13804c2ace72ae8c92f8b9e7452410b
apumjp0508 <akihisatengshan@gmail.com>
2025-01-24 11:58:03 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+       
+       function sort_2way($array, $order) {
+        if ($order === 'asc') {
+            sort($array);
+        } elseif ($order === 'desc') {
+            rsort($array);
+        }
+        
+        foreach($array as $value) {
+            echo "$value";
+            echo "<br>";
+        }
+    }
+    
+    $array_data = [15, 4, 18, 23, 10];
+    
+    echo "昇順にソートします。<br>";
+    sort_2way($array_data, 'asc');
+    
+    echo "降順にソートします。<br>";
+    sort_2way($array_data, 'desc');
+    
+
+            
+        ?>
+    </p>
+</body>
+
+</html>
